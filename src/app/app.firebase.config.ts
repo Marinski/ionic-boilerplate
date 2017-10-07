0501277942e567b5ee95320e117f94e684f32e36
Marinski <marinski@home>
2017-10-07 21:00:27 +0300

    Update firebase config for github

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBRFq2CZM7OG5TbGGlJXnesdfcm8h2UQco",
-    authDomain: "arboreal-inn-110720.firebaseapp.com",
-    databaseURL: "https://arboreal-inn-110720.firebaseio.com",
-    projectId: "arboreal-inn-110720",
-    storageBucket: "arboreal-inn-110720.appspot.com",
-    messagingSenderId: "188489375037"
-}
