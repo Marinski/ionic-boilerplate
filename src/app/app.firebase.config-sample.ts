26fae9cd32d0e0f6678e3697280cf5d80a183275
Marin Stoyanov <marin_bs@abv.bg>
2017-10-07 20:57:33 +0300

    Delete core config file for GitHub

--- src/app/app.firebase.config-sample.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "copy and paste your Firebase config object",
-    authDomain: "copy and paste your Firebase config object",
-    databaseURL: "copy and paste your Firebase config object",
-    projectId: "copy and paste your Firebase config object",
-    storageBucket: "copy and paste your Firebase config object",
-    messagingSenderId: "copy and paste your Firebase config object"
-}
